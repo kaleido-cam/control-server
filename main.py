45668749d4c2b9a76e5e7c6a8a02fd6d25726efa
Soeren Wegener <wegener92@gmail.com>
2025-12-12 00:43:06 +0100

    Init

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from gpiozero import LED, OutputDevice
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# configure motor speed
+# motor = LED(4)
+# motor.blink(on_time=0.0001, off_time=0.0001)
+#
+# motor_enable = OutputDevice(27)
+
+@app.get("/on")
+def motor_on():
+    # Inverted pin
+    motor_enable.off()
+
+@app.get("/off")
+def motor_off():
+    # Inverted pin
+    motor_enable.on()
